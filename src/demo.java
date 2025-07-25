ba9ad6b5e90f60ad6d2e424f36a0adc320d02024
Dreamless07 <saadkhan7903@gmail.com>
2025-07-25 11:06:21 +0530

    added a connection with create and execute

--- src/demo.java
+++ src/demo.java
@@ -1,11 +1,20 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.Connection;//inbuilt
+import java.sql.DriverManager;//inbuilt
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class demo {
-    public static void main(String[] args) {
-        String url;
-        String username="root";
-        String password="5.somethinG";
-        Connection con= DriverManager.getConnection(url,username,password);
+    public static void main(String[] args) throws Exception{//add exception
+        String sql="select cname from customers where cid=1";//a query from the db
+        String url="jdbc:mysql://localhost:3306/zeptoDB";// connect jdbc to your database;
+        String username="root";//your db username
+        String password="5.somethinG";//your db password
+        Connection con= DriverManager.getConnection(url,username,password);//create var, Connection is interface therefore cannot create object, so access through a inbuilt class(DriverManager) which contains inbuilt method(getConnection)
+        Statement st=con.createStatement();// create inbuilt interface and store in var, connection with create statement
+        st.executeQuery(sql);//execute the statement
+        ResultSet rs=st.executeQuery(sql);// create inbuilt interface and store in rs, execute the query
+        rs.next();//change line
+        String name= rs.getString(1);//get the value of first column
+        System.out.println(name);//print
     }
 }
